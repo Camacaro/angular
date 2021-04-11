e3fe5a39858112046912f7a58d69676ad43ea2ba
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-11 07:12:13 -0400

    one way data

--- 01-bases/src/app/heroes/heroe/heroe.component.ts
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -16,4 +16,12 @@ export class HeroeComponent {
   obtenerNombre(): string {
     return `${this.nombre} - ${this.edad}`;
   }
+
+  cambiarNombre(): void {
+    this.nombre = 'Spiderman';
+  }
+
+  cambiarEdad(): void {
+    this.edad = 30;
+  }
 }
