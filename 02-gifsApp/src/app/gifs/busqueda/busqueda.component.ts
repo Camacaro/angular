3c0f24cbac6d34dfd514476572298d24f2ba99b8
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-11 13:30:35 -0400

    viewChild

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  // !: Non-null assertion operator (typescript) -> asegurarse que el elemento no es nulo
+  // Buscar por referencia local - Busca en el HTML que quiera
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  // Foma  de hacerlo sin usar formModule or ngModule
+  buscar(): void{
+    console.log();
+
+    const value = this.txtBuscar.nativeElement.value;
+
+    console.log(value);
+
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
