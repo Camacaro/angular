87b977b30aaefed906f811f6376b1af45896d9c2
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-11 14:45:08 -0400

    http in resultados

--- 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadosComponent implements OnInit {
 
-  constructor() { }
+  get resultados(): any[] {
+    return this.gifsService.resultados;
+  }
+
+  constructor(private gifsService: GifsService) { }
 
   ngOnInit(): void {
   }
--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -8,6 +8,7 @@ export class GifsService {
 
   private apiKey = 'DJFlzUwZU7oceIPxuPKI2siknP7pFqlZ';
   private _historial: string[] = [];
+  public resultados: any[] = [];
 
   get historial(): string[]  {
     return [...this._historial];
@@ -25,9 +26,11 @@ export class GifsService {
       this._historial = this._historial.slice(0, 10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=DJFlzUwZU7oceIPxuPKI2siknP7pFqlZ&q=one piece&limit=10`)
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      // tslint:disable-next-line: deprecation
       .subscribe( (resp: any) => {
         console.log(resp.data)
+        this.resultados = resp.data;
       });
   }
 }
