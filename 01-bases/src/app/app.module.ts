e120d49df7af1d51ccfccb87e0f8b618ab3a94c2
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-11 07:05:58 -0400

    template heroe

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContatorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContatorComponent
+    ContatorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
