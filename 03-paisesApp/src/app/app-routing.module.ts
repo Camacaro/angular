5f3cd818d422889f36786923ae966445f1854bfb
Jesus Camacaro <jesus.camacaro@imageimaker.com>
2021-04-12 17:01:55 -0400

    routing

---
+++ 03-paisesApp/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    pathMatch: 'full', // para que solo use /
+  },
+  {
+    path: 'region',
+    component: PorRegionComponent
+  },
+  {
+    path: 'capital',
+    component: PorCapitalComponent
+  },
+  {
+    path: 'pais/:id',
+    component: VerPaisComponent
+  },
+  {
+    path: '**', // Si no encuentra ninguna redirecciona
+    redirectTo: '',
+    // component o para mostrar un 404
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {}
--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { PaisModule } from './pais/pais.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
-    PaisModule
+    PaisModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
