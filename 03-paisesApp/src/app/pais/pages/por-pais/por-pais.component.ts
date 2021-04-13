bf629d1674a1daf78db207b2166ab524d26020e7
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-13 11:28:33 -0400

    handle Error

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,14 +10,27 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino = '';
+  hayError = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(): void {
+    this.hayError = false;
+
     this.paisService.buscarPais( this.termino )
-      .subscribe( resp => {
-        console.log(resp);
-      });
+      .subscribe(
+        (resp) => {
+          console.log(resp);
+        },
+        (err) => {
+          this.hayError = true;
+        }
+      );
+
+    // this.paisService.buscarPais( this.termino )
+    //   .subscribe({
+    //     next: (resp) => console.log(resp)
+    //   });
   }
 
 }
--- 03-paisesApp/src/app/pais/services/pais.service.ts
+++ 03-paisesApp/src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,11 @@ export class PaisService {
   buscarPais(termino: string): Observable<any> {
     const url = `${ this.apiUrl }/name/${ termino }`;
     return this.http.get( url );
+
+    // return this.http.get( url )
+    //   .pipe(
+    //     // atrapa el error y devuelve un observable array vacio
+    //     catchError( err => of([]) )
+    //   );
   }
 }
