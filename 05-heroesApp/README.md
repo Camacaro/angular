e7982b59b3a2aadd4e5cc16d3b9328fc337be4d2
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-17 18:52:27 -0400

    maqueta home

--- 05-heroesApp/README.md
+++ 05-heroesApp/README.md
@@ -3,3 +3,12 @@
 * Rutas Principales
 * LazyLoad
 * Multiples estilos en la misma SPA
+* Angular Material
+* Interfaces y tipado
+* Pipes personalizados
+* Variables de entorno
+* Autocomplete de AngularMaterial
+* Peticiones HTTP
+* JSON-Server
+* Angular Flex y Flexbox
+* Y más...
--- 05-heroesApp/src/app/heroes/heroes.module.ts
+++ 05-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Modules
+import { HeroeRoutingModule } from './heroe-routing.module';
+import { MaterialModule } from '../material/material.module';
+// Components
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroeRoutingModule } from './heroe-routing.module';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -19,7 +22,9 @@ import { HeroeRoutingModule } from './heroe-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroeRoutingModule
+    FlexLayoutModule,
+    HeroeRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
