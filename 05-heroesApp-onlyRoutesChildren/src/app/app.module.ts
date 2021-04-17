863186c7391b541ee3ea0d22d0aff3c7ccacfa9d
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-17 18:06:07 -0400

    app routes children

---
+++ 05-heroesApp-onlyRoutesChildren/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
