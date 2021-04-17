7fef2373feb9588f7471efa463e0650f94b6ee8b
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-17 18:04:20 -0400

    router-outlet children

--- 05-heroesApp/src/app/heroes/heroe-routing.module.ts
+++ 05-heroesApp/src/app/heroes/heroe-routing.module.ts
@@ -4,10 +4,15 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+
+// Con agregar component: HomeComponent, no va mostrar las rutas se va directo hacia 
+// el home y hay que aplicar el router outlet para que lo muestre
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
