b5010f9beebbb52b545f53ff088ee0fd25870627
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-19 17:03:39 -0400

    environmnetS

--- 05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`);
   }
 }
