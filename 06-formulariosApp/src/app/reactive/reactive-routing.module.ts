236e739a3f567f491102c31c1bfd0fc23de6a95d
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-20 17:33:39 -0400

    modules and routings

---
+++ 06-formulariosApp/src/app/reactive/reactive-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  // exports: [RouterModule]  lo quito porque esta exportado de manera global
+})
+export class ReactiveRoutingModule { }
