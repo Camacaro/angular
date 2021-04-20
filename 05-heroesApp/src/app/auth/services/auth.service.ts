86595af8040c5c3040a23bb52e6f9ad1e8f0cb9e
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-20 16:15:11 -0400

    Auth service

--- 05-heroesApp/src/app/auth/services/auth.service.ts
+++ 05-heroesApp/src/app/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Auth } from '../interfaces/auth.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  login(): Observable<Auth> {
+    return this.http.get<Auth>( `${ this.baseUrl }/usuarios/1` );
+  }
 }
