bfe8dbfebd404b6a717020defd10a557f1de9871
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-20 16:27:54 -0400

    get auth

--- 05-heroesApp/src/app/auth/services/auth.service.ts
+++ 05-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Auth } from '../interfaces/auth.interface';
 
 @Injectable({
@@ -10,10 +11,19 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth { // | undefined
+    // return this._auth ? { ...this._auth } : undefined;
+    return { ...this._auth! };
+  }
 
   constructor(private http: HttpClient) { }
 
   login(): Observable<Auth> {
-    return this.http.get<Auth>( `${ this.baseUrl }/usuarios/1` );
+    return this.http.get<Auth>( `${ this.baseUrl }/usuarios/1` )
+      .pipe(
+        tap( auth => this._auth = auth )
+      );
   }
 }
