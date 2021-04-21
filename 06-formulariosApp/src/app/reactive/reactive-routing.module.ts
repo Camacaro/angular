20d66b841267e3ffe569b24e3b6a3cac327af318
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-21 09:43:36 -0400

    componentes y LazyLoad

--- 06-formulariosApp/src/app/reactive/reactive-routing.module.ts
+++ 06-formulariosApp/src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basicos',
+        component: BasicosComponent
+      },
+      {
+        path: 'dinamicos',
+        component: DinamicosComponent
+      },
+      {
+        path: 'switches',
+        component: SwitchesComponent
+      },
+      {
+        path: '**',
+        component: BasicosComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
