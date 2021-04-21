59a86259b0a1fd15fe2c20c5ca119efb92fe7a7c
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-21 09:50:56 -0400

    routes lazyload

--- 06-formulariosApp/src/app/app-routing.module.ts
+++ 06-formulariosApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( module => module.TemplateModule )
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( module => module.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
