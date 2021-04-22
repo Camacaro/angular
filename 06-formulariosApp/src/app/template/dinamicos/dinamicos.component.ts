3ed46178e650748c9e92ac8eca54e94ec92ed72d
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-22 02:26:12 -0400

    agregar y eliminar items del formulario

--- 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  // @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  ngOnInit(): void {
+  nuevoJuego = '';
+
+  persona: Persona = {
+    nombre: 'Jesus',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gaer'
+      },
+      {
+        id: 1,
+        nombre: 'Death Stranding'
+      },
+    ]
+  };
+
+
+  guardar(): void {
+    console.log('postetado');
+  }
+
+  eliminar( index: number ): void {
+    // Borrar 1, el indice de de donde se mando
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego(): void {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+
+    this.persona.favoritos.push( {...nuevoFavorito} );
+
+    this.nuevoJuego = '';
   }
 
 }
