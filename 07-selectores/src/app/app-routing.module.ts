712dbd325d24e9f9a16585f293c80b44722ecdfa
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-27 12:08:09 -0400

    structure paises

--- 07-selectores/src/app/app-routing.module.ts
+++ 07-selectores/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'selector',
+    loadChildren:  () => import('./paises/paises.module').then( m => m.PaisesModule )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
