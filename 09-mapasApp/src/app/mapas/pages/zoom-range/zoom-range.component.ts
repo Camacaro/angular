edb910d3fb61c1d7b4f7efcab735a61c71de2ad2
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-27 19:50:01 -0400

    remove listeners and move on

--- 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -24,7 +24,7 @@ import * as mapboxgl from 'mapbox-gl';
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   // Uso la referencia porque con el id si tengo mas referencia a esta componente
   // se ira repitiendo el id, con la referencia angular se encarga de qeu este componente
@@ -33,8 +33,16 @@ export class ZoomRangeComponent implements AfterViewInit {
 
   mapa!: mapboxgl.Map;
   zoomLevel = 10;
+  center: [number, number] = [ -70.6584204903743, -33.461761703487504 ];
 
   constructor() {}
+
+  ngOnDestroy(): void {
+    // remove the listeners
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   // Se usa este porque necesitas esperar que la referencia se encuentre lista
   // para ser usado y con este metodo del ciclo de vida lo podemos obtener
@@ -44,7 +52,7 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -70.6584204903743, -33.461761703487504 ],
+      center: this.center,
       zoom: this.zoomLevel
     });
 
@@ -57,6 +65,12 @@ export class ZoomRangeComponent implements AfterViewInit {
       if ( this.mapa.getZoom() > 18 ) {
         this.zoomLevel = 18;
       }
+    });
+
+    // Moviento del mapa
+    this.mapa.on('move', (ev) => {
+      const { lng, lat } = ev.target.getCenter();
+      this.center = [lng, lat];
     });
   }
 
