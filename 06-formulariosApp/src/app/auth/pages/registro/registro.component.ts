3e60cfb09c3a2640c853583e86d4bf3f64a87d42
Jesus Camacaro <jalejcamacaro@falabella.cl>
2021-04-27 09:45:10 -0400

    process validators

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // de la "a" miniscula a la "z" miniscula
+  // de la "A" mayucula a la "Z" mayucula
+  // + cualquier caracter adicional
+  nombreApellidoPattern = '([a-zA-Z]+) ()';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [ Validators.required, Validators.pattern(this.nombreApellidoPattern) ] ]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
 }
+
