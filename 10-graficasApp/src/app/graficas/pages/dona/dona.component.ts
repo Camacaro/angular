1e2d5f836832580d1cf9275986845ceceb48c486
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-28 12:42:57 -0400

    donas

--- 10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent  {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100, 250]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
-  }
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#4DCEF8',
+        '#43D9D6',
+        '#56F0C2',
+        '#43D982',
+        '#4DF864'
+      ]
+    }
+  ]
 
 }
