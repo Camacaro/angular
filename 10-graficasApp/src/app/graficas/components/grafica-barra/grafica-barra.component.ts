567337355f3666b2f7c7225ecb1df511907f1ba9
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-28 12:35:52 -0400

    grafica-barra

--- 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ 10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @Input() horizontal = false;
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  @Input() barChartLabels: Label[] = [];
+    // ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  @Input() barChartData: ChartDataSets[] = [];
+    // [
+    //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#FF8324', hoverBackgroundColor: 'red' },
+    //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    //   { data: [12, 43, 3, 12, 32, 1, 54], label: 'Series C' }
+    // ];
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar';
+    }
   }
 
 }
