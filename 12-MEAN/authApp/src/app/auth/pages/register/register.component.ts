80c056ae514925393543e3aadb69503c0ae314b3
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-04-29 10:30:29 -0400

    router navigate

--- 12-MEAN/authApp/src/app/auth/pages/register/register.component.ts
+++ 12-MEAN/authApp/src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,11 +16,16 @@ export class RegisterComponent {
     password: ['123456', [Validators.required, Validators.minLength(6)]]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   registro(): void {
     console.log(this.miFormulario.value)
     console.log(this.miFormulario.valid)
+
+    this.router.navigateByUrl('/dashboard');
   }
 
 }
