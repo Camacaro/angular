209f9cf6f6b5b26b2531874c964170c8f156053e
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-06-01 10:27:41 -0400

    angular in public node

--- 12-MEAN/auth-server/index.js
+++ 12-MEAN/auth-server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./db/config');
@@ -15,6 +16,11 @@ app.use( cors() );
 app.use( express.json() );
 
 app.use( '/api/auth', require('./routes/auth') );
+
+// Manejar demas rutas para el angualr public y no usar el #
+app.get( '*', (req, res) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+})
 
 app.listen( process.env.PORT, () => {
   console.log(`Servidor Corriendo en el puerto ${ process.env.PORT }`);
--- 12-MEAN/authApp/src/app/app-routing.module.ts
+++ 12-MEAN/authApp/src/app/app-routing.module.ts
@@ -19,8 +19,23 @@ const routes: Routes = [
   }
 ];
 
+/**
+ * El useHash: true
+ * Se usa cuando por ejemplo estoy en apache y no puedo
+ * tocar el .htaccess para configurar que el index manejara 
+ * las rutas, entonces aplico el useHash: true
+ * 
+ * Esto solucion es para navegadores viejos o cuando no tenemos 
+ * acceso a las rutas del apache en el .htaccess
+ * 
+ * 
+ * Lo deje en false porque no quiero el # en la URL y el cambio
+ * lo hare en el backend de node 
+ */
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
